07444effe3ae4d09d34b14fbc67aae887637276b
franco039 <francoemanuel039@gmail.com>
2022-07-18 12:38:31 -0300

    Google Sign added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express'); 
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -14,5 +14,12 @@ router.post('/login',[
     validarCampos
 ], login);
 
+router.post('/google',[
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSingIn);
+
 
 module.exports = router;
+
+
